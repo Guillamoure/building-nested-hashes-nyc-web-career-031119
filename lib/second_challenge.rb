ad11b7f4acbba3c7ddc28d94a7b90185b36aecbf
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-02-26 04:54:13 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,15 +2,15 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []
       }, 
     :capulet => {
       #your key/value pairs here!
-      :patriarch => {}
-      :matriarch => {}
+      :patriarch => {},
+      :matriarch => {},
       :heroine => {}
       :heroine_friends => []
       }}
